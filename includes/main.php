2f6f6835ad2a4881efada698c5697da6368dc4fb
Riky Ripaldo <rikyripaldo@icloud.com>
2026-07-04 12:33:31 +0900

    second

---
+++ includes/main.php
@@ -0,0 +1,9 @@
+<div class="container mt-4">
+  <div class="p-5 mb-4 bg-primary text-white rounded">
+    <div class="container-fluid py-5">
+      <h1 class="display-5 fw-bold">Portal Berita Modern</h1>
+      <p class="col-md-8 fs-4">Menyajikan berita terbaru, terpercaya dan terkini</p>
+      <button class="btn btn-warning btn-lg">Baca berita</button>
+    </div>
+  </div>
+</div>
---
+++ includes/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand fw-bold" href="#">News Portal</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navigasi">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div id="navigasi" class="collapse navbar-collapse">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Kategori</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Tentang</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link btn btn-warning ms-2 px-3" href="#">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
